950cc3bc754a818d996618f01b3ffdedd6831296
Hughie Mak <hymak@qbssystem.com>
2019-07-03 12:47:08 +0800

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { User } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    // First Validate The HTTP Request
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    //  Now find the user by their name
+    let user = await User.findOne({ name: req.body.name });
+    if (!user) {
+        return res.status(400).send('Incorrect name or password.');
+    }
+
+    // Then validate the Credentials in MongoDB match
+    // those provided in the request
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+        return res.status(400).send('Incorrect name or password.');
+    }
+ 
+    res.send(_.pick(user, ['_id', 'name']));
+});
+
+function validate(req) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        password: Joi.string().min(5).max(255).required()
+    };
+ 
+    return Joi.validate(req, schema);
+}
+
+module.exports = router;
